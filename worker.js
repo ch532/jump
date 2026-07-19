d91bffe4272e79cb0bf7310a7eae149e56ded826
ch532 <trustconnect713@gmail.com>
2026-07-19 07:12:52 +0100

    Refactor wasm_calculate_layout to capture intent

--- worker.js
+++ worker.js
@@ -1,14 +1,9 @@
-function wasm_calculate_layout(hold_duration, target) {
-    let mode = "DEFAULT", action = "NONE";
-
-    if (hold_duration > 3000) { mode = "SYSTEM_CONFIG"; } 
-    else if (hold_duration > 1500) { mode = "DEEP_FOCUS"; action = "OPEN_MENU"; } 
-    else if (hold_duration > 500) { mode = "EXPLORATION_FLOW"; }
-
-    return { mode, target, action };
-}
-
-self.onmessage = (e) => {
-    const { dwellDuration, target } = e.data.data;
-    self.postMessage({ type: 'APPLY_LAYOUT', layout: wasm_calculate_layout(dwellDuration, target) });
+// worker.js - Synthesis Engine for Cross-Browser SDK
+self.onmessage = function(e) {
+    if (e.data.type === 'CAPTURE_INTENT') {
+        const { dwellDuration } = e.data.data;
+        if (dwellDuration >= 800) { // 800ms threshold
+            self.postMessage({ type: 'APPLY_LAYOUT', layout: { action: 'OPEN_MENU' } });
+        }
+    }
 };

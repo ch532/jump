8a7c85c607a9a661ff1408ce972d09cc02a88a28
ch532 <trustconnect713@gmail.com>
2026-07-19 14:49:11 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,10 @@
-// worker.js - Intelligence Engine
+// worker.js
 self.onmessage = function(e) {
-    // 1. Feature Discovery Report
+    // Discovery Engine
     if (e.data.type === 'ANALYZE_PAGE') {
         self.postMessage({ type: 'INTELLIGENCE_REPORT', recommendations: e.data.features });
     }
-    
-    // 2. Menu Trigger Logic
+    // Menu Trigger Logic
     if (e.data.type === 'CAPTURE_INTENT' && e.data.data.dwellDuration >= 800) {
         self.postMessage({ type: 'APPLY_LAYOUT', layout: { action: 'OPEN_MENU' } });
     }
